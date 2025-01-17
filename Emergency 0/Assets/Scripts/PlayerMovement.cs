cc0299689654911c6d5ce240fa60a623adf0dfce
David <david.vrckovic.dv@gmail.com>
2025-01-17 18:54:46 +0100

    Test 1

---
+++ Emergency 0/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public string test = "Test";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Test 1
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Test 2
+    }
+}
